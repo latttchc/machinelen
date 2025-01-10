850fe888ec3d01b9c9ea2e3bc576c561bcd7bd3c
latttchc <iam.yuk2882@gmail.com>
2025-01-10 16:17:41 +0900

    word2id の辞書を用いて単語を数 値に変換して数値化したリストを作成

--- neko-mecab2.py
+++ neko-mecab2.py
@@ -15,7 +15,27 @@ for i in range(num):
                 id = id + 1
                 word2id[word] = id #word to id
                 id2word[id] = word #id to word
-
 f.close()
 print("word2id:",word2id)
 print("id2word:",id2word)
+
+def w2id(word2id, length):
+    result = []
+    f = open("wakati.txt", "r")
+    text = f.readlines()
+    for i in range(len(text)):
+        tmp = []
+        for word in text[i].split(" "):
+            if word in word2id:
+                tmp.append(word2id[word])
+            if word == "\n":
+                num = len(tmp)
+                if num < length:
+                    for i in range(length - num):
+                        tmp.append(0)
+                result.append(tmp)
+    f.close()
+    return result
+
+id_list = w2id(word2id, 30)
+print(id_list)
